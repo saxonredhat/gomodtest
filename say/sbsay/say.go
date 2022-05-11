0dceced9966755bb3a875716cc899233112ddaa0
saxon.redhat <saxon.redhat@gmail.com>
2022-05-12 02:49:48 +0800

    add sbsay

---
+++ say/sbsay/say.go
@@ -0,0 +1,8 @@
+package sbsay 
+
+import "fmt" 
+
+// say Hi to someone
+func SBSay(){
+   fmt.Printf("I AM SB")
+}
---
+++ say/sbsay/say2.go
@@ -0,0 +1,8 @@
+package sbsay 
+
+import "fmt" 
+
+// say Hi to someone
+func SBSay2(){
+   fmt.Printf("I AM SB 2")
+}
